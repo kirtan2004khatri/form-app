622b79107b04cafda220317010f13ffad56117b4
kirtan <kirtan2004khatri@gmail.com>
2022-06-18 17:00:29 +0530

    This is the form tutorial in vue js

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <SignUpForm />
+</template>
+
+<script>
+import SignUpForm from './components/SignUpForm.vue'
+export default {
+  name: 'App',
+  components: {
+    SignUpForm
+  }
+}
+</script>
+
---
+++ src/components/SignUpForm.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="container border mt-5 w-25 p-5">
+        <form>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <!-- Here you can use v-model:"variable name" to track change in it which are stored and tracked by a vairable -->
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                    v-model="email">
+                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" v-model="pwd">
+            </div>
+            <div class="mb-3 form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1" v-model="check">
+                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            </div>
+            <div class="mb-3">
+                <label for="disabledSelect" class="form-label">Role:</label>
+                <select id="disabledSelect" class="form-select" v-model="role">
+                    <option>Python</option>
+                    <option>Java</option>
+                    <option>JavaScript</option>
+                    <option>C#</option>
+                </select>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+    <h1 class="text-center">{{ email }} and {{ pwd }} and {{role}} and check {{check}}</h1>
+</template>
+
+<script>
+export default {
+    name: "SignUpForm",
+    data() {
+        return {
+            //These two variables are tracking the changes in form
+            email: "123@gmail.com", pwd: "123456",role:"Python",check:false
+            // By entering values in this tracking variables you are setting default values in form
+        }
+    }
+}
+</script>

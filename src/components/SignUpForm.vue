03ccc2b5f2d0093235c4dd16835499c684052b9e
kirtan <kirtan2004khatri@gmail.com>
2022-06-18 17:32:25 +0530

    Second commit

--- src/components/SignUpForm.vue
+++ src/components/SignUpForm.vue
@@ -16,6 +16,18 @@
                 <input type="checkbox" class="form-check-input" id="exampleCheck1" v-model="check">
                 <label class="form-check-label" for="exampleCheck1">Check me out</label>
             </div>
+            <div class="mb-3 form-check">
+                <input type="checkbox" class="form-check-input" value="cooking" v-model="hobbies">Cooking
+            </div>
+              <div class="mb-3 form-check">
+                <input type="checkbox" class="form-check-input" value="dancing" v-model="hobbies">Dancing
+            </div>
+              <div class="mb-3 form-check">
+                <input type="checkbox" class="form-check-input" value="singing" v-model="hobbies">Singing
+            </div>
+              <div class="mb-3 form-check">
+                <input type="checkbox" class="form-check-input" value="reading" v-model="hobbies">Reading
+            </div>
             <div class="mb-3">
                 <label for="disabledSelect" class="form-label">Role:</label>
                 <select id="disabledSelect" class="form-select" v-model="role">
@@ -25,10 +37,14 @@
                     <option>C#</option>
                 </select>
             </div>
+            <div class="mb-3">
+                <label for="skills" class="form-label">Skills</label>
+                <input type="text" name="skills" class="form-control" v-model="tempSkills" @keyup="addSkills">
+            </div>
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
     </div>
-    <h1 class="text-center">{{ email }} and {{ pwd }} and {{role}} and check {{check}}</h1>
+    <h1 class="text-center">{{ email }} and {{ pwd }} and {{role}} and check {{check}} and array {{hobbies}}</h1>
 </template>
 
 <script>
@@ -37,8 +53,15 @@ export default {
     data() {
         return {
             //These two variables are tracking the changes in form
-            email: "123@gmail.com", pwd: "123456",role:"Python",check:false
+            email: "123@gmail.com", pwd: "123456",role:"Python",check:false,hobbies:[],
             // By entering values in this tracking variables you are setting default values in form
+            tempSkills:"",skills:[]
+        }
+    },
+    method:{
+        addSkills(e){
+            console.log(e);
+            console.log('hii')
         }
     }
 }
